54f47ebe17876a4e956ba255b60915a0fc70aff4
wierus-ps <pstanek18@gmail.com>
2024-11-14 16:52:33 +0100

    Initial commit

---
+++ src/test/java/com/spacex/dragonsstatus/DragonsstatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacex.dragonsstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonsstatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
